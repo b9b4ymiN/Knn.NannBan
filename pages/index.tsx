f76c684c5862987b9989e45eed667c58fec72913
b9b4ymiN <pro.thitipat@gmail.com>
2023-04-30 10:38:58 +0700

    commit for first publish

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,89 +1,89 @@
-import { Fancybox } from "@fancyapps/ui";
+import type { Fancybox as FancyboxType } from "@fancyapps/ui/types";
+import * as Fancyapps from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import Link from "next/link";
+import { useEffect, useRef } from "react";
+import { NextPage } from "next";
 
-const IndexPage = () => (
-  <>
-    <div className="header">
-      <Link className="logo" href={"/home"}>
-        <img
-          className="blue"
-          src="../assets/img/logo_knn.png"
-          alt="logo"
-          style={{ opacity: 0 }}
-        />
-        <img
-          className="write"
-          src="../assets/img/logo_knn.png"
-          alt="logo write"
-          style={{ opacity: 1 }}
-        ></img>
-      </Link>
-      <h1 className="txtHeadEN ani300">
-         A BETTER LIFE FOR OURS SOCIETY
-      </h1>
-      <span className="lineHead">|</span>
-      <div className="txtHeadTH">
-        <span>โคกหนองนา </span>อาจารย์<span> หนาน บาล</span>
+const IndexPage: NextPage = (props) => {
+ 
+
+  return (
+    <>
+      <div className="header">
+        <Link className="logo" href={"/"}>
+          <img
+            className="blue"
+            src="../assets/img/logo_knn.png"
+            alt="logo"
+            style={{ opacity: 0 }}
+          />
+          <img
+            className="write"
+            src="../assets/img/logo_knn.png"
+            alt="logo write"
+            style={{ opacity: 1 }}
+          ></img>
+        </Link>
+        <h1 className="txtHeadEN ani300">A BETTER LIFE FOR OUR SOCIETY</h1>
+        <span className="lineHead">|</span>
+        <div className="txtHeadTH">
+          <span>โคกหนองนา </span>อาจารย์<span> หนาน บาล</span>
+        </div>
+        <div className="headMenu ani300"></div>
+        <div className="boxListIcon">
+          <Link href={"#"} className="btnSitemap">
+            sitmap
+          </Link>
+          <Link className="btnHome" href="#/home">
+            home
+          </Link>
+        </div>
       </div>
-      <div className="headMenu ani300"></div>
-      <div className="boxListIcon">
-        <Link href={"#"} className="btnProject">
-          project
-        </Link>
-        <Link href={"#"} className="btnSitemap">
-          sitmap
-        </Link>
-        <Link className="btnHome" href="#/home">
-          home
-        </Link>
-      </div>
-    </div>
-    {/* Home */}
-    <div
-      id="home"
-      className="boxPage"
-      style={{
-        transform: "translate3d(0px, 0%, 0px)",
-        display: "block",
-        height: "100%",
-        overflow: "hidden",
-        position: "fixed",
-      }}
-    >
+      {/* Home */}
       <div
-        className="listBgPage"
-        style={{ transform: "translate3d(0px, 0%, 0px)" }}
+        id="home"
+        className="boxPage"
+        style={{
+          transform: "translate3d(0px, 0%, 0px)",
+          display: "block",
+          height: "100%",
+          overflow: "hidden",
+          position: "fixed",
+        }}
       >
-        <div className="oneBgPage homeBG1"></div>
-      </div>
-      <div
-        className="contentPage"
-        style={{ transform: "translate3d(0px, 0%, 0px)" }}
-      >
-        <h2 className="titlePage">เ พื่ อ ส ร้ า ง ชี วิ ต ที่ พ อ เ พี ย ง</h2>
-        <div className="boxContentCtrl"></div>
-        <div className="boxDetailMain">
-          <div className="boxDetail">
-            <div className="bgBoxDetail"></div>
-            <div className="bdrBoxDetail"></div>
-            <div className="listDetail">
-              <div className="detail" style={{marginTop : '15px'}}>
-                
-                มุ่งมั่นให้ทุกคนสามารถพึ่งพาตัวเองได้
-                <br />
-                ด้วยปรัชญาเศรษฐกิจพอเพียง
+        <div className="listBgPage showTranForm">
+          <div className="oneBgPage homeBG1"></div>
+        </div>
+        <div
+          className="contentPage"
+          style={{ transform: "translate3d(0px, 0%, 0px)" }}
+        >
+          <h2 className="titlePage">
+            เ พื่ อ ส ร้ า ง ชี วิ ต ที่ พ อ เ พี ย ง
+          </h2>
+          <div className="boxContentCtrl"></div>
+          <div className="boxDetailMain">
+            <div className="boxDetail">
+              <div className="bgBoxDetail"></div>
+              <div className="bdrBoxDetail"></div>
+              <div className="listDetail">
+                <div className="detail" style={{ marginTop: "15px" }}>
+                  มุ่งมั่นให้ทุกคนสามารถพึ่งพาตัวเองได้
+                  <br />
+                  ด้วยปรัชญาเศรษฐกิจพอเพียง
+                </div>
               </div>
             </div>
           </div>
+          <Link className="btnEnter" href="#/about">
+            <img src="/assets/img/home/btnEnter.png" alt="" /> เข้าสู่เว็บไซต์
+          </Link>
         </div>
-        <Link className="btnEnter" href="#/about">
-          <img src="/assets/img/home/btnEnter.png" alt="" /> เข้าสู่เว็บไซต์
-        </Link>
       </div>
-    </div>
-    {/* End Home */}
-  </>
-);
+      {/* End Home */}
+    </>
+  );
+};
 
 export default IndexPage;
