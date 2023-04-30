9b64b59513e0938cfc75f53326c06d7ce0436976
b9b4ymiN <pro.thitipat@gmail.com>
2023-04-30 10:18:11 +0700

    add css and image than first page

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from "next/app";
+import "../public/assets/css/cookie.css";
+import "../public/assets/css/responsive.css";
+import "../public/assets/css/share.css";
+import "../public/assets/css/custom.css"
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
